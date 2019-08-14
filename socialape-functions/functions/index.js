1bfb238a30554ce73acb1277f5f068b6573650da
david <davidediak@gmail.com>
2019-08-14 13:02:31 +0200

    getting and commenting on scream

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -1,7 +1,12 @@
 const functions = require('firebase-functions');
 const app = require('express')();
 const FBAuth = require('./util/fbAuth');
-const { getAllScreams, postOneScream } = require('./handlers/screams');
+const {
+  getAllScreams,
+  postOneScream,
+  getScream,
+  postScreamComment
+} = require('./handlers/screams');
 const {
   signup,
   login,
@@ -13,10 +18,13 @@ const {
 //Scream routes
 app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postOneScream);
-app.post('/user', FBAuth, addUserDetails);
-app.get('/user', FBAuth, getAuthenticatedUser);
+app.get('/scream/:screamId', getScream);
+app.post('/scream/:screamId/comment', FBAuth, postScreamComment);
 
 app.post('/signup', signup);
 app.post('/login', login);
 app.post('/user/image', FBAuth, uploadImage);
+app.post('/user', FBAuth, addUserDetails);
+app.get('/user', FBAuth, getAuthenticatedUser);
+
 exports.api = functions.region('europe-west1').https.onRequest(app);
